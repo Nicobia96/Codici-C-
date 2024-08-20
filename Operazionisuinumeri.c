fd39c188845951d9c2ec83259a158845a4c1e77f
nicolo biasio <nbiasio0@gmail.com>
2024-08-20 09:16:40 -0400

    codiciC++

---
+++ Operazionisuinumeri.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/*
+* Addizione es: c = a + b
+* Sottrazione es: c = a - b
+* Moltiplicazione es c = a * b
+* Divsione es c = a / b
+* Modulo es c = 11 % 2 (c = 1 di resto)
+* Incremento a++ e' un esemplificazione di  a = a + 1 
+* Decremento a-- e' un esemplificazione di a = a -1
+*/
+
+ int main (){
+ int a = 0;
+     a++;
+     printf("Valore di a = %d\n", a);
+ }
